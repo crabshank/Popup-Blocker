951a29e1c2ca4981087607450dac61b6a6523a5a
crabshank <44329154+crabshank@users.noreply.github.com>
2020-11-15 19:45:38 +0000

    Add files via upload

--- background.js
+++ background.js
@@ -1,5 +1,20 @@
+function getUrl(tab){
+	return (tab.url=="")?tab.pendingUrl:tab.url;
+}
+
 chrome.windows.onCreated.addListener((window) => {
 	if (window.type==='popup'){
-	 chrome.windows.remove(window.id);
+		chrome.tabs.query({windowId: window.id}, function(tabs) {
+			let xmp=false;
+			for (let t = 0; t < tabs.length; t++) {
+			if(getUrl(tabs[t]).startsWith('chrome-extension://')){
+				xmp=true;
+				break;
+			}
+		}
+			if(!xmp){
+			chrome.windows.remove(window.id);
+			}
+			});
 	}
 });
