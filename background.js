810a824a5747d524eadd6730b049ef1bf469ef96
crabshank <44329154+crabshank@users.noreply.github.com>
2021-03-16 02:37:55 +0000

    Add files via upload

--- background.js
+++ background.js
@@ -1,16 +1,6 @@
 try {
 function getUrl(tab){
-	if(tab.pendingUrl!=null){
-	return (tab.url=="")?tab.pendingUrl:tab.url;
-	}else{
-		
-		if(tab.url!=null){
-			return tab.url;
-		}else{
-			return tab; //if tab.url not there
-		}
-	
-	}
+	return (tab.url=="" && typeof tab.pendingUrl!=='undefined' && tab.pendingUrl!='')?tab.pendingUrl:tab.url;
 }
 
 chrome.windows.onCreated.addListener((window) => {
