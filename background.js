f5373dda69a2508c15cbea61bd12378d31e5acbf
crabshank <44329154+crabshank@users.noreply.github.com>
2021-06-21 21:46:04 +0100

    Add files via upload

--- background.js
+++ background.js
@@ -1,10 +1,63 @@
+try{
+var lifeline;
+
+keepAlive();
+
+chrome.runtime.onConnect.addListener((port)=> {
+  if (port.name === 'keepAlive') {
+    lifeline = port;
+    setTimeout(keepAliveForced, 295e3); // 5 minutes minus 5 seconds	
+    port.onDisconnect.addListener(keepAliveForced);
+  }
+});
+
+function keepAliveForced() {
+  lifeline?.disconnect();
+  lifeline = null;
+  keepAlive();
+}
+
+async function keepAlive() {
+ // if (lifeline) return;
+  for (var tab of await chrome.tabs.query({ url:"*://*/*"})) {
+    try {
+							chrome.scripting.executeScript({
+								  target: {tabId: tab.id, allFrames: true},
+								  files: ['port_connect.js'],
+								}, () => {});
+	  console.log(lifeline);
+      chrome.tabs.onUpdated.removeListener(retryOnTabUpdate);
+	  chrome.tabs.onReplaced.addListener(retryOnTabUpdate2);
+	  chrome.tabs.onRemoved.addListener(retryOnTabUpdate3);
+      return;
+    } catch (e) {;}
+  }
+  chrome.tabs.onUpdated.addListener(retryOnTabUpdate);
+	  chrome.tabs.onReplaced.addListener(retryOnTabUpdate2);
+	  chrome.tabs.onRemoved.addListener(retryOnTabUpdate3);
+}
+
+async function retryOnTabUpdate(tabId, info, tab) {
+  if (info.url) {
+    keepAlive();
+  }
+}
+async function retryOnTabUpdate2(addedTabId, removedTabId) {
+    keepAlive();
+}
+async function retryOnTabUpdate3(tabId, removeInfo) {
+    keepAlive();
+}
+	/*Source: https://stackoverflow.com/a/66618269 - wOxxOm*/
+	
+}catch(e){;}
+
 try {
 function getUrl(tab){
 	return (tab.url=="" && !!tab.pendingUrl && typeof tab.pendingUrl!=='undefined' && tab.pendingUrl!='')?tab.pendingUrl:tab.url;
 }
 
 function windowProc(window){
-		return new Promise((resolve, reject)=>{
 		if (window.type==='popup'){
 		chrome.tabs.query({windowId: window.id}, function(tabs) {
 			let xmp=false;
@@ -15,20 +68,16 @@ function windowProc(window){
 			}
 		}
 			if(!xmp){
-			chrome.windows.remove(window.id,function() {resolve();});
-			}else{resolve();}
+			chrome.windows.remove(window.id);
+			}
 			});
 	}
-	resolve();
-});
 }
 
 function handleMessage(request, sender, sendResponse) {
-			return new Promise((resolve, reject)=>{
 	chrome.windows.get(sender.tab.windowId,(window) => {
-		windowProc(window);
+		windowProc(window)
 	});
-			});
 }
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
