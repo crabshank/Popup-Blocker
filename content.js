bdb351556094dd49a04830524633b8766d8e9b40
crabshank <44329154+crabshank@users.noreply.github.com>
2021-11-04 06:03:37 +0000

    Add files via upload

--- content.js
+++ content.js
@@ -1,12 +1,7 @@
 try{
-	let puTb=`window.open = () => {};`;
-
-	if([...window.document.head.getElementsByTagName('script')].filter((tg)=>{return tg.innerHTML===puTb}).length==0)
-	{
-		let scr=window.document.createElement('script');
-		scr.innerHTML=puTb;
-		window.document.head.insertAdjacentElement('afterbegin',scr);
-	}
+	let scr = document.createElement('script');
+	scr.src = chrome.runtime.getURL('tab_block.js');
+	window.document.head.insertAdjacentElement('afterbegin',scr);
 }catch(e){;}
 
 chrome.runtime.sendMessage({msg:window.location.href}, function(response){});
