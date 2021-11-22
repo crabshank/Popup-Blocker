4134150d28d1a00daa068be9bc32a25fba19b7e9
crabshank <44329154+crabshank@users.noreply.github.com>
2021-11-22 00:51:53 +0000

    Add files via upload

--- content.js
+++ content.js
@@ -1,7 +1,3 @@
 try{
-	let scr = document.createElement('script');
-	scr.src = chrome.runtime.getURL('tab_block.js');
-	window.document.head.insertAdjacentElement('afterbegin',scr);
-}catch(e){;}
-
 chrome.runtime.sendMessage({msg:window.location.href}, function(response){});
+}catch(e){;}
