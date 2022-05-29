140cee1a5e8986e70b8f0f56e44e088ecaf5f4c9
crabshank <44329154+crabshank@users.noreply.github.com>
2022-05-29 01:20:35 +0100

    Add files via upload

--- background.js
+++ background.js
@@ -284,7 +284,9 @@ function windowProc(window){
 }
 
 function handleMessage(request, sender, sendResponse) {
+	let sr=true;
 	if(request.type=="get_info"){
+		sr=false;
 		sendResponse({info: sender});
 	}else if(request.type=="links"){
 		let tbl=tb_links.findIndex((t)=>{return t[0]==sender.tab.id;});
@@ -311,11 +313,14 @@ function handleMessage(request, sender, sendResponse) {
 	}else if(request.type=="nav"){
 		url_upd(sender.tab,request.new_url);
 	}
+	
+	if(sr){
+		sendResponse({response: "Message received"});
+	}
 }
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	handleMessage(request, sender, sendResponse);
-	sendResponse({response: "Message received"});
 });
 
 chrome.windows.onCreated.addListener((window) => {
