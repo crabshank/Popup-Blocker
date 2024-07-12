922ec6a04ce13871a3e42ca78da75fb45460e61f
crabshank <44329154+crabshank@users.noreply.github.com>
2024-07-12 17:24:21 +0100

    Add files via upload

--- content.js
+++ content.js
@@ -52,6 +52,9 @@ function setupEvents(){
 				if(lc && !altLc){
 					let nm=( e.shiftKey && (!(e.shiftKey && ( e.ctrlKey || e.altKey ) )) )?'':'_blank'; //new window is ''
 					nm=( e.shiftKey || e.ctrlKey  )?nm:'_self';
+					if(!t.href.startsWith('data:')){
+						window.open(t.href, nm);
+					}
 				}
 			}
 		}
